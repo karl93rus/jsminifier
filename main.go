69124c628d92ad5a6b9dfa2b06af135b75d280e1
Alex Sazonov <karl93rus@gmail.com>
2019-08-05 21:12:37 +0300

    Base CLI interface provided

--- main.go
+++ main.go
@@ -6,12 +6,66 @@ import (
   "os"
   "strings"
   "math/rand"
+  "flag"
+  "io"
 )
 
 func main() {
-  r := bufio.NewScanner(os.Stdin)
+
+  var (
+    path string
+    src  string
+    help bool
+  )
+
+  flag.StringVar(&path, "path", "./", "Specify output path")
+  flag.StringVar(&src, "src", "", "Specify input file")
+  flag.BoolVar(&help, "h", false, "Usage:")
+
+  flag.Parse()
+
+  var reader io.Reader
+
+  if help == true {
+    fmt.Println("USAGE:\t jsminifier -src \"[INPUT FILE]\" -p \"[OUTPUT FILE]\"\n")
+    fmt.Println("\t FLAGS: -src    : Input file\n\t      \t-path   : Output file")
+    return
+  }
+
+  if src == "" {
+    a, err := os.Stdin.Stat()
+    if err != nil {
+      fmt.Println("Error")
+    }
+    if ((a.Mode() & os.ModeNamedPipe) == 0) {
+      fmt.Println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+      fmt.Println("SRC not defined. Use -h for help of use pipline input.")
+      fmt.Println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n")
+      return
+    } else {
+      fmt.Println("Input source: STDIN")
+      reader = os.Stdin
+    }
+  } else {
+    fl, err := os.Open(src)
+    if err != nil {
+      fmt.Println("Error opening file...")
+      return
+    }
+    defer fl.Close()
+    reader = fl
+  }
+
+  var output string
+  if path == "./" {
+    output = path + "minified.js"
+  } else {
+    output = path
+  }
+
   var res string
   varMap := make(map[string]string)
+  r := bufio.NewScanner(reader)
 
   for r.Scan() {
     line := r.Text()
@@ -33,7 +87,7 @@ func main() {
     res = strings.Replace(res, k, v, -1)
   }
 
-  f, err := os.Create("./script.js")
+  f, err := os.Create(output)
   defer f.Close()
   if err != nil {
     fmt.Println("Error...")
